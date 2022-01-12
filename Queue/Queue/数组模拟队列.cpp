64b61fcef668b8b39b9b6c3f07b1bbfa043fc290
HGG-Git-hub <8023hgg@gmail.com>
2022-01-12 17:57:54 +0800

    拨开迷雾见明月

---
+++ Queue/Queue/数组模拟队列.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+const int N = 100010;
+
+int q[N], hh, tt=-1,n;
+
+int main() 
+{
+	cin >> n;
+	while (n--) 
+	{
+		string str;
+		int x;
+		if (str == "push")
+		{
+			cin >> x;
+			q[++tt] = x;
+		}
+		else if (str == "pop")
+		{
+			hh++;
+		}
+		else if (str == "query")
+		{
+			cout << q[hh] << endl;
+		}
+		else
+			cout << (hh <= tt ? "NO" : "YES") << endl;
+	}
+
+	system("pause");
+	return 0;
+};
