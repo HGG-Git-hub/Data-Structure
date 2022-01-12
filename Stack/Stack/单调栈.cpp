acf5b314125d7e79e3b2671b133308bc36396e7a
HGG-Git-hub <8023hgg@gmail.com>
2022-01-12 18:45:18 +0800

    拨开迷雾见明月

---
+++ Stack/Stack/单调栈.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+const int N = 100010;
+//n个数，数组，栈顶指针
+int n, stk[N], tt;
+
+int main() 
+{
+	std::cin.tie(0);
+	std::ios::sync_with_stdio(false);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+	{
+		int x;
+		cin >> x;
+		while (tt && stk[tt] > x) //非空且栈里元素大于输入
+		{
+			tt--;
+		}
+		if (tt)//栈非空
+		{
+			cout << stk[tt] << ' ';
+		}
+		else//栈空
+		{
+			cout << -1 << ' ';
+		}	
+		stk[++tt] = x;
+	}
+	system("pause");
+	return 0;
+};
