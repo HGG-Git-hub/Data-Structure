61582ee3a5f6a3b9e59bf179d10b0fcbc4fc914e
HGG-Git-hub <8023hgg@gmail.com>
2022-01-14 15:23:06 +0800

    拨开迷雾见明月

---
+++ KMP/KMP/KMP字符串.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+const int M = 100010, N = 10010;
+char s[M], p[N];
+int n, m;
+
+int ne[N];
+int main() 
+{
+	std::cin.tie(0);
+	std::ios::sync_with_stdio(false);
+
+	cin >> n >> p + 1 >> m >> s + 1;
+	//求net的过程
+	for (int i = 2, j = 0; i <= n; i++)
+	{
+		while (j && p[i] != p[j + 1]) 
+			j = ne[j];
+		if (p[i] == p[j + 1]) 
+			j++;
+		ne[i] = j;
+	}
+	//kmp匹配过程
+	for (int i = 1, j = 0; i <= m; i++) 
+	{
+		while (j && s[i] != p[j + 1]) 
+			j = ne[j];
+		if (s[i] == p[j + 1]) 
+			j++;
+		if (j == n) 
+		{
+			cout << i - n << " ";
+			j = ne[j];
+		}
+	}
+	system("pause");
+	return 0;
+};
