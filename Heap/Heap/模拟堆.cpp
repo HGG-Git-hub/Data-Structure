5b10699f8f73bcd3bc3ec0f9ae2e5af6acf6c07e
HGG-Git-hub <8023hgg@gmail.com>
2022-01-15 18:33:47 +0800

    拨开迷雾见明月

---
+++ Heap/Heap/模拟堆.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 100010;
+
+int main() 
+{
+	std::cin.tie(0);
+	std::ios::sync_with_stdio(false);
+
+	system("pause");
+	return 0;
+};
