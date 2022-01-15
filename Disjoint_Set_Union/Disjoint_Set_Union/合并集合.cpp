d6fcb3eaa169fe88cae554830c07dfdc7ba41dc2
HGG-Git-hub <8023hgg@gmail.com>
2022-01-15 10:44:22 +0800

    拨开迷雾见明月

---
+++ Disjoint_Set_Union/Disjoint_Set_Union/合并集合.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+
+const int N = 100010;
+
+int n,m;
+int p[N];//存储每个节点的父节点
+
+int find(int x) //返回x所在集合的编号，即根节点，包含了路径优化
+{
+	if (p[x] != x)
+		p[x] = find(p[x]);
+	return p[x];
+}
+int main() 
+{
+	std::cin.tie(0);
+	std::ios::sync_with_stdio(false);
+	
+	cin >> n >> m;
+	/*最开始，每一个元素都是一个集合，对应在树中，即只有根节点
+	根节点也是自己，即p[i]=[i];*/
+	for (int i = 1; i <= n; i++) p[i] = i;
+	while (m--) 
+	{
+		char op[2];
+		int a, b;
+		cin >> op >> a >> b;
+		if (op[0] == 'M') //合并操作
+			p[find(a)] = find(b);
+		else 
+			cout << (find(a) == find(b) ? "Yes" : "No") << endl;
+	}
+	system("pause");
+	return 0;
+};
---
+++ Disjoint_Set_Union/Disjoint_Set_Union/源.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int a = 1;
+	int b = 2;
+	a = b;
+	cout << a << endl;
+	cout << b;
+	return 0;
+};
