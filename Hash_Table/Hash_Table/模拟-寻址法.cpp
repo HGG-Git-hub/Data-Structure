5a789e1c386f6afe15bc4b72fc0c209fc01f3fc1
HGG-Git-hub <8023hgg@gmail.com>
2022-01-16 17:22:40 +0800

    拨开迷雾见明月

--- Hash_Table/Hash_Table/模拟-寻址法.cpp
+++ Hash_Table/Hash_Table/模拟-寻址法.cpp
@@ -1,17 +1,43 @@
 ﻿#include<iostream>
 #include<cstring>
 using namespace std;
+//数组里的某个位置等于null=0x3f3f3f3f说明数组的这个位置没有数字
+const int N = 200003, null = 0x3f3f3f3f;
+int h[N];//槽，被映射
 
-const int N = 100003;
-
+int find(int x) 
+{
+	int k = (x % N + N) % N;
+	while (h[k] != null && h[k] != x) //有数据且不等于x
+	{
+		k++;//看下一个位置
+		//k=N,表示已经查完了整个数组，需要从数组0的位置从新开始
+		if (k == N) k = 0;
+	}
+	//k表示如果当前位置有元素返回下个位置的地址
+	//当前位置没有元素，返回当前元素位置的地址
+	return k;
+}
 
 int main() 
 {
 	std::cin.tie(0);
 	std::ios::sync_with_stdio(false);
 
-
-
+	int n;
+	cin >> n;
+	memset(h, 0x3f, sizeof(h));//初始化h数组
+	while (n--)
+	{
+		string s;
+		int x;
+		cin >> s >> x;
+		int k = find(x);
+		if (s == "I")
+			h[k] = x;
+		else
+			cout << (h[k] != null ? "Yes" : "No") << endl;
+	}
 	system("pause");
 	return 0;
 };
